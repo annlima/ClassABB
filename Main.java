9f852731e3201ac9bdda8e6bd4f89c7c32d4905e
JoseAA03P <91348516+JoseAA03P@users.noreply.github.com>
2023-10-11 20:09:34 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,10 +23,28 @@ public class Main {
                     int capacidad = scanner.nextInt();
                     scanner.nextLine();  // Consumir el salto de línea
 
-                    System.out.print("Ingrese los detalles del aula (ej. Edificio A - Aula 101): ");
-                    String detalles = scanner.nextLine();
+                    System.out.print("Ingrese los detalles del aula: ");
+                    String descripcion = scanner.nextLine();
+                    scanner.nextLine();  // Consumir el salto de línea
 
-                    abb.insert(capacidad, detalles);
+
+                    System.out.print("Ingreseel edificio del aula: ");
+                    String edificio = scanner.nextLine();
+                    scanner.nextLine();  // Consumir el salto de línea
+
+                    System.out.print("Ingrese el numero del aula: ");
+                    int salon = scanner.nextInt();
+
+                    System.out.print("Ingrese 0 si quiere no quiere pc y 1 si si ");
+                    int pc = scanner.nextInt();
+
+                    System.out.print("Ingrese 0 si quiere no quiere proyector y 1 si si ");
+                    int proyector = scanner.nextInt();
+
+                    System.out.print("Ingrese 0 si quiere no quiere pizarron y 1 si si ");
+                    int pizarron = scanner.nextInt();
+
+                    abb.insert(capacidad, edificio, salon, descripcion, pc, proyector, pizarron);
                     System.out.println("Aula ingresada con éxito!");
                     break;
 
@@ -34,7 +52,16 @@ public class Main {
                     System.out.print("Ingrese la capacidad de estudiantes para la clase: ");
                     int capacidadEstudiantes = scanner.nextInt();
 
-                    String aulaAsignada = abb.assignAula(capacidadEstudiantes);
+                    System.out.print("Ingrese si quiere pc: ");
+                    int pcaula = scanner.nextInt();
+
+                    System.out.print("Ingrese la capacidad de estudiantes para la clase: ");
+                    int proyectoraula = scanner.nextInt();
+
+                    System.out.print("Ingrese la capacidad de estudiantes para la clase: ");
+                    int pizarronaula = scanner.nextInt();
+
+                    String aulaAsignada = abb.assignAula(capacidadEstudiantes, pcaula, proyectoraula, pizarronaula);
                     if (aulaAsignada != null) {
                         System.out.println("Aula asignada: " + aulaAsignada);
                     } else {
