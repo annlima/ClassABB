b57c776e80401a5c7d745bf0a9c45520a7bdb111
JoseAA03P <91348516+JoseAA03P@users.noreply.github.com>
2023-10-11 20:10:41 -0600

    Update Aula.java

--- Aula.java
+++ Aula.java
@@ -1,13 +1,25 @@
 // Modelo de una clase Aula
 class Aula {
     int capacidad; // Capacidad máxima de estudiantes que puede albergar el aula
-    String detalles; // Detalles del aula, como su ubicación
+    String edificio; // Edificio del salon
+    int salon; //Numero de salon del edificio
+    String descripcion; // Descripcion del salon
+    int pc;
+    int proyector;
+    int pizarron;
+
 
     Aula() {} // Constructor vacío
 
     // Constructor con parámetros
-    Aula(int capacidad, String detalles) {
+    Aula(int capacidad, String edificio,int salon, String descripcion, int pc, int proyector, int pizarron) {
         this.capacidad = capacidad;
-        this.detalles = detalles;
+        this.edificio = edificio;
+        this.salon = salon;
+        this.descripcion = descripcion;
+        this.pc = pc;
+        this.proyector = proyector;
+        this.pizarron = pizarron;
+
     }
 }
